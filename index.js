b86197fbf1fb9ebde896ad7a857dcd9eaf97da47
abhishek9918 <ojha.abhishek312@gmail.com>
2024-12-02 10:56:40 +0530

    ignore node modules

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require("http");
+const colors = require("colors");
 
 function dataControl(req, resp) {
   resp.write("<h1>hello !, this is abhishek ojha </h1>");
   resp.end();
 }
 
+console.log("package.json".red);
 http.createServer(dataControl).listen(1100);
